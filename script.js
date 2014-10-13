2965c9b7f4c193d00aaa7343e2cbb7e04e23ef1b
sofia.larborn@gmail.com <Morvar>
2014-10-13 10:19:53 +0200

    hitbox och allt

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //deklarera variabler
-var c, ctx, bollX = 100, bollY = 100, bollVX = 1, bollVY = 2;
+var c, ctx, bollX = 100, bollY = 100, bollVX = -1, bollVY = 2;
 var leftPlY = 100, rightPlY = 200, leftPlVY = 0, rightPlVY = 0;
 
+//skaffa canvas
 function init () {
     c = document.getElementById("duk");
     ctx = c.getContext("2d");
@@ -43,16 +44,61 @@ function update(){
         bollVY = -bollVY;
         bollY = 0;
     }
+    //studs mot spelare left
+    if(bollX > 10 && bollX < 30 && bollY > leftPlY && bollY < leftPlY + 50){
+        bollVX = bollVX * (-1);
+    }
+    //studs mot spelare right
+    if(bollX > 370 && bollX < 390 && bollY > rightPlY && bollY < rightPlY + 50){
+        bollVX = bollVX * (-1);
+    }
+    
+    //hindra rutorna från att fly
+    if(leftPlY < -25){
+    leftPlVY = 0;
+    leftPlY = -25;
+    }
+    if(leftPlY > 275){
+    leftPlVY = 0;
+    leftPlY = 275;
+    }
+    if(rightPlY < -25){
+    rightPlVY = 0;
+    rightPlY = -25;
+    }
+    if(rightPlY > 275){
+    rightPlVY = 0;
+    rightPlY = 275;
+    }
+    
 
 }
-
-function keyDown(e){
-    //knapptryck uppåt 38
-    if(e.keyCode == 38){
+//funktionen som behandlar alla knapptryckningar
+object.onkeypress = function keyPress(e){
+    //vänster knapptryck uppåt w
+    if(e.keyCode == 87){
         leftPlVY = -2;
     }
-    if(e.keyCode == 40){
+    
+    //vänster neråt s
+    else if(e.keyCode == 83){
         leftPlVY = 2;
     }
+    else{
+        leftPlVY = 0;
+    }
+    //höger uppåt up
+    if(e.keyCode == 38){
+        rightPlVY = -2;
+    }
+    //höger neråt down
+    else if(e.keyCode == 40){
+        rightPlVY = 2;
+    }
+    else{
+        rightPlVY = 0;
+    }
+    
+    //kolla keyID
     console.log(e.keyCode);
 }
